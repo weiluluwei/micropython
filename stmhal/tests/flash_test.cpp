55f1701c26244b652178e61b33f99bc3377cbcd3
Tobias Badertscher <python@baerospace.ch>
2016-02-09 18:07:55 +0100

    Finished setup of unittest frame for flash by adding a moch object for HAL functions.

--- stmhal/tests/flash_test.cpp
+++ stmhal/tests/flash_test.cpp
@@ -4,6 +4,14 @@
  *  Created on: 09.02.2016
  *      Author: badi
  */
+#include <stdint.h>
+#include <stdio.h>
+extern "C"
+{
+    #include "flash.h"
+}
+
+//CppUTest includes should be after your and system includes
 #include "CppUTest/TestHarness.h"
 
 TEST_GROUP(FlashTestGroup)
@@ -11,7 +19,11 @@ TEST_GROUP(FlashTestGroup)
 
 };
 
-TEST(FlashTestGroup, GetSector)
+TEST(FlashTestGroup, GetFirstSector)
 {
-   FAIL("Fail me!");
+    uint32_t myAddr = 0x08000000+42;
+    uint32_t start_addr;
+    uint32_t size;
+    uint32_t sec = flash_get_sector_info(myAddr, &start_addr, &size);
+    printf("sector 0x%08x", sec);
 }
