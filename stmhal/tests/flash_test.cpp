19f86f1e7ac5a846c0ad2885db3aebbff9287cc3
Tobias Badertscher <python@baerospace.ch>
2016-02-09 17:31:11 +0100

    Added unit test for flash refactoring.

---
+++ stmhal/tests/flash_test.cpp
@@ -0,0 +1,17 @@
+/*
+ * test_flash.c
+ *
+ *  Created on: 09.02.2016
+ *      Author: badi
+ */
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(FlashTestGroup)
+{
+
+};
+
+TEST(FlashTestGroup, GetSector)
+{
+   FAIL("Fail me!");
+}
