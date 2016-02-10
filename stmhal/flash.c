d8005a553f85217a595fbad910892b4839e0a8aa
Tobias Badertscher <python@baerospace.ch>
2016-02-10 18:49:47 +0100

    Refactoring for f4 series of flash_get_sector_info succeeds all tests.

--- stmhal/flash.c
+++ stmhal/flash.c
@@ -30,110 +30,67 @@
 #include "mpconfigport.h"
 #include "py/misc.h"
 
-#if defined(MCU_SERIES_F7)
+typedef struct {
+    uint32_t base_address;
+    uint32_t sector_size;
+    uint32_t sector_count;
+} flash_organization_t;
 
+#if defined(MCU_SERIES_F4)
+static const flash_organization_t flash_organisation[]= {
+        {((uint32_t)0x08000000), 16384,   4 },
+        {((uint32_t)0x08010000), 0x10000, 1 },
+        {((uint32_t)0x08020000), 0x20000, 3 },
+#if defined(FLASH_SECTOR_8)
+        {((uint32_t)0x08080000), 0x20000, 4 },
+#endif
+#if defined(FLASH_SECTOR_12)
+        {((uint32_t)0x08100000), 16384,   4 },
+        {((uint32_t)0x08110000), 0x10000, 1 },
+        {((uint32_t)0x08120000), 0x20000, 7 }
+#endif
+};
+#elif defined(MCU_SERIES_F7)
 // FLASH_FLAG_PGSERR (Programming Sequence Error) was renamed to
 // FLASH_FLAG_ERSERR (Erasing Sequence Error) in STM32F7
 #define FLASH_FLAG_PGSERR FLASH_FLAG_ERSERR
 
-/* Base address of the Flash sectors */
-#define ADDR_FLASH_SECTOR_0     ((uint32_t)0x08000000) /* Base @ of Sector 0, 32 Kbytes */
-#define ADDR_FLASH_SECTOR_1     ((uint32_t)0x08008000) /* Base @ of Sector 1, 32 Kbytes */
-#define ADDR_FLASH_SECTOR_2     ((uint32_t)0x08010000) /* Base @ of Sector 2, 32 Kbytes */
-#define ADDR_FLASH_SECTOR_3     ((uint32_t)0x08018000) /* Base @ of Sector 3, 32 Kbytes */
-#define ADDR_FLASH_SECTOR_4     ((uint32_t)0x08020000) /* Base @ of Sector 4, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_5     ((uint32_t)0x08040000) /* Base @ of Sector 5, 256 Kbytes */
-#define ADDR_FLASH_SECTOR_6     ((uint32_t)0x08080000) /* Base @ of Sector 6, 256 Kbytes */
-#define ADDR_FLASH_SECTOR_7     ((uint32_t)0x080C0000) /* Base @ of Sector 7, 256 Kbytes */
-#define ADDR_FLASH_END          ((uint32_t)0x08100000) /* 1 Mbytes total */
-
-#else
-
-/* Base address of the Flash sectors */
-#define ADDR_FLASH_SECTOR_0     ((uint32_t)0x08000000) /* Base @ of Sector 0, 16 Kbytes */
-#define ADDR_FLASH_SECTOR_1     ((uint32_t)0x08004000) /* Base @ of Sector 1, 16 Kbytes */
-#define ADDR_FLASH_SECTOR_2     ((uint32_t)0x08008000) /* Base @ of Sector 2, 16 Kbytes */
-#define ADDR_FLASH_SECTOR_3     ((uint32_t)0x0800C000) /* Base @ of Sector 3, 16 Kbytes */
-#define ADDR_FLASH_SECTOR_4     ((uint32_t)0x08010000) /* Base @ of Sector 4, 64 Kbytes */
-#define ADDR_FLASH_SECTOR_5     ((uint32_t)0x08020000) /* Base @ of Sector 5, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_6     ((uint32_t)0x08040000) /* Base @ of Sector 6, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_7     ((uint32_t)0x08060000) /* Base @ of Sector 7, 128 Kbytes */
-#if !defined(FLASH_SECTOR_8)
-#define ADDR_FLASH_END          ((uint32_t)0x08080000) /* 512 Kbytes total */
-#else
-#define ADDR_FLASH_SECTOR_8     ((uint32_t)0x08080000) /* Base @ of Sector 8, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_9     ((uint32_t)0x080A0000) /* Base @ of Sector 9, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_10    ((uint32_t)0x080C0000) /* Base @ of Sector 10, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_11    ((uint32_t)0x080E0000) /* Base @ of Sector 11, 128 Kbytes */
-#if !defined(FLASH_SECTOR_12)
-#define ADDR_FLASH_END          ((uint32_t)0x08100000) /* 1 Mbytes total */
-#else
-#define ADDR_FLASH_SECTOR_12    ((uint32_t)0x08100000) /* Base @ of Sector 12,  16 Kbytes */
-#define ADDR_FLASH_SECTOR_13    ((uint32_t)0x08104000) /* Base @ of Sector 13,  16 Kbytes */
-#define ADDR_FLASH_SECTOR_14    ((uint32_t)0x08108000) /* Base @ of Sector 14,  16 Kbytes */
-#define ADDR_FLASH_SECTOR_15    ((uint32_t)0x0810C000) /* Base @ of Sector 15,  16 Kbytes */
-#define ADDR_FLASH_SECTOR_16    ((uint32_t)0x08110000) /* Base @ of Sector 16,  64 Kbytes */
-#define ADDR_FLASH_SECTOR_17    ((uint32_t)0x08120000) /* Base @ of Sector 17, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_18    ((uint32_t)0x08140000) /* Base @ of Sector 18, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_19    ((uint32_t)0x08160000) /* Base @ of Sector 19, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_20    ((uint32_t)0x08180000) /* Base @ of Sector 20, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_21    ((uint32_t)0x081A0000) /* Base @ of Sector 21, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_22    ((uint32_t)0x081C0000) /* Base @ of Sector 22, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_23    ((uint32_t)0x081E0000) /* Base @ of Sector 23, 128 Kbytes */
-#define ADDR_FLASH_END          ((uint32_t)0x08200000) /* 2 Mbytes total */
-#endif
-#endif
-
-#endif // MCU_SERIES_F7
-
-static const uint32_t flash_info_table[] = {
-    ADDR_FLASH_SECTOR_0, FLASH_SECTOR_0,
-    ADDR_FLASH_SECTOR_1, FLASH_SECTOR_1,
-    ADDR_FLASH_SECTOR_2, FLASH_SECTOR_2,
-    ADDR_FLASH_SECTOR_3, FLASH_SECTOR_3,
-    ADDR_FLASH_SECTOR_4, FLASH_SECTOR_4,
-    ADDR_FLASH_SECTOR_5, FLASH_SECTOR_5,
-    ADDR_FLASH_SECTOR_6, FLASH_SECTOR_6,
-    ADDR_FLASH_SECTOR_7, FLASH_SECTOR_7,
-    #if defined(FLASH_SECTOR_8)
-    ADDR_FLASH_SECTOR_8, FLASH_SECTOR_8,
-    ADDR_FLASH_SECTOR_9, FLASH_SECTOR_9,
-    ADDR_FLASH_SECTOR_10, FLASH_SECTOR_10,
-    ADDR_FLASH_SECTOR_11, FLASH_SECTOR_11,
-    #endif
-    #if defined(FLASH_SECTOR_12)
-    ADDR_FLASH_SECTOR_12, FLASH_SECTOR_12,
-    ADDR_FLASH_SECTOR_13, FLASH_SECTOR_13,
-    ADDR_FLASH_SECTOR_14, FLASH_SECTOR_14,
-    ADDR_FLASH_SECTOR_15, FLASH_SECTOR_15,
-    ADDR_FLASH_SECTOR_16, FLASH_SECTOR_16,
-    ADDR_FLASH_SECTOR_17, FLASH_SECTOR_17,
-    ADDR_FLASH_SECTOR_18, FLASH_SECTOR_18,
-    ADDR_FLASH_SECTOR_19, FLASH_SECTOR_19,
-    ADDR_FLASH_SECTOR_20, FLASH_SECTOR_20,
-    ADDR_FLASH_SECTOR_21, FLASH_SECTOR_21,
-    ADDR_FLASH_SECTOR_22, FLASH_SECTOR_22,
-    ADDR_FLASH_SECTOR_23, FLASH_SECTOR_23,
-    #endif
-    ADDR_FLASH_END, 0,
+static const flash_organization_t flash_organisation[]= {
+        {((uint32_t)0x08000000), 32768,   4 },
+        {((uint32_t)0x08020000), 0x20000, 1 },
+        {((uint32_t)0x08040000), 0x40000, 3 },
 };
+#elif defined(MCU_SERIES_L4)
+static const flash_organization_t flash_organisation[]= {
+        {((uint32_t)0x08000000), 2048,  512 }
+};
+#else
+#error "UNKONWN Processor"
+#endif
+
 
 uint32_t flash_get_sector_info(uint32_t addr, uint32_t *start_addr, uint32_t *size) {
-    if (addr >= flash_info_table[0]) {
-        for (int i = 0; i < MP_ARRAY_SIZE(flash_info_table) - 2; i += 2) {
-            if (addr < flash_info_table[i + 2]) {
-                if (start_addr != NULL) {
-                    *start_addr = flash_info_table[i];
+    if (addr >= flash_organisation[0].base_address) {
+        uint32_t sector_index = 0;
+        for (int i = 0; i < MP_ARRAY_SIZE(flash_organisation); i++) {
+            for (int j = 0; j< flash_organisation[i].sector_count; j++){
+                uint32_t sector_start_next =  flash_organisation[i].base_address+((j+1)*flash_organisation[i].sector_size);
+                if (addr < sector_start_next) {
+                    if (start_addr != NULL) {
+                        *start_addr = flash_organisation[i].base_address+j*flash_organisation[i].sector_size;
+                    }
+                    if (size != NULL) {
+                        *size = flash_organisation[i].sector_size;
+                    }
+                    return sector_index;
                 }
-                if (size != NULL) {
-                    *size = flash_info_table[i + 2] - flash_info_table[i];
-                }
-                return flash_info_table[i + 1];
+                sector_index++;
             }
         }
     }
     return 0;
 }
+
 
 void flash_erase(uint32_t flash_dest, const uint32_t *src, uint32_t num_word32) {
     // check there is something to write
