035bf4a6832517b65ffcb32a0a3adbbb427a7554
Tobias Badertscher <python@baerospace.ch>
2016-03-30 10:35:07 +0200

    Adapted mpconfigboard.h to LimiFrog Board.

--- stmhal/boards/LIMIFROG/mpconfigboard.h
+++ stmhal/boards/LIMIFROG/mpconfigboard.h
@@ -26,12 +26,14 @@
 #define MICROPY_HW_FLASH_LATENCY    FLASH_LATENCY_4
 
 // USART config
-#define MICROPY_HW_UART2_PORT (GPIOD)
-#define MICROPY_HW_UART2_PINS (GPIO_PIN_5 | GPIO_PIN_6)
+#define MICROPY_HW_UART3_PORT (GPIOC)
+#define MICROPY_HW_UART3_PINS (GPIO_PIN_10 | GPIO_PIN_11)
 
 // I2C busses
-#define MICROPY_HW_I2C1_SCL (pin_B6)
-#define MICROPY_HW_I2C1_SDA (pin_B7)
+#define MICROPY_HW_I2C1_SCL (pin_B8)
+#define MICROPY_HW_I2C1_SDA (pin_B9)
+#define MICROPY_HW_I2C2_SCL (pin_B10)
+#define MICROPY_HW_I2C2_SDA (pin_B11)
 // We use an array of baudrates and corresponding TIMINGR values.
 //
 // The value 0x90112626 was obtained from the DISCOVERY_I2C1_TIMING constant
@@ -41,20 +43,22 @@
 #define MICROPY_HW_I2C_BAUDRATE_MAX     100000
 
 // SPI busses
-#define MICROPY_HW_SPI2_NSS     (pin_D0)
-#define MICROPY_HW_SPI2_SCK     (pin_D1)
-#define MICROPY_HW_SPI2_MISO    (pin_D3)
-#define MICROPY_HW_SPI2_MOSI    (pin_D4)
+#define MICROPY_HW_SPI1_NSS     (pin_A4)
+#define MICROPY_HW_SPI1_SCK     (pin_A5)
+#define MICROPY_HW_SPI1_MISO    (pin_A6)
+#define MICROPY_HW_SPI1_MOSI    (pin_A7)
 
-// Joystick is pulled low. Pressing the button makes the input go high.
-#define MICROPY_HW_USRSW_PIN        (pin_A0)
+#define MICROPY_HW_SPI3_SCK     (pin_B3)
+#define MICROPY_HW_SPI3_MISO    (pin_B4)
+#define MICROPY_HW_SPI3_MOSI    (pin_B5)
+
+#define MICROPY_HW_USRSW_PIN        (pin_A15)
 #define MICROPY_HW_USRSW_PULL       (GPIO_NOPULL)
 #define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_RISING)
 #define MICROPY_HW_USRSW_PRESSED    (1)
 
 // LEDs
-#define MICROPY_HW_LED1             (pin_B2) // red
-#define MICROPY_HW_LED2             (pin_E8) // green
+#define MICROPY_HW_LED1             (pin_C3) // red
 #define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
 #define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRR = pin->pin_mask)
 #define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRR = pin->pin_mask<<16)
