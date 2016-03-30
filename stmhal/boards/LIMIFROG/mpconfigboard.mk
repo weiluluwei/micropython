1c985760b60ea57117f1f1ae08b896677535318c
Tobias Badertscher <python@baerospace.ch>
2016-03-30 09:49:00 +0200

    Added Limifrog specific make target deploy2b to store firmware0/1 on the board via STLINK.

--- stmhal/boards/LIMIFROG/mpconfigboard.mk
+++ stmhal/boards/LIMIFROG/mpconfigboard.mk
@@ -2,3 +2,9 @@ MCU_SERIES = l4
 CMSIS_MCU = STM32L476xx
 AF_FILE = boards/stm32l476_af.csv
 LD_FILE = boards/stm32l476xe.ld
+
+deploy2b: $(BUILD)/firmware.dfu
+	$(ECHO) "Writing $(BUILD)/firmware0.bin to the board via ST-LINK"
+	$(Q)$(STFLASH) write $(BUILD)/firmware0.bin 0x08000000
+	$(ECHO) "Writing $(BUILD)/firmware1.bin to the board via ST-LINK"
+	$(Q)$(STFLASH) --reset write $(BUILD)/firmware1.bin 0x08008800
